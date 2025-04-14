b47e646b748ea71d36a6629205cd10a8374ed35e
HaeungLee <smanew@hotmail.com>
2025-04-14 14:08:38 +0900

    html 되는버전

--- src/main/java/com/bbs/demo/HelloController.java
+++ src/main/java/com/bbs/demo/HelloController.java
@@ -1,17 +1,17 @@
-package com.bbs.demo;
+//package com.bbs.demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
 
-@Controller
-public class HelloController {
+//@Controller
+//public class HelloController {
 
-    @GetMapping("/")
-    public String index() {
+//    @GetMapping("/")
+//    public String index() {
         //return "Hello, 1조!";
-    	return "index"; //templates/index.html 랜더링
-    }
-}
+//    	return "index"; //templates/index.html 랜더링
+//    }
+//}
