66709d6fe57d9c771d78e1e3e20bfb8aa3a84b57
HaeungLee <smanew@hotmail.com>
2025-04-14 14:04:03 +0900

    html실행 파일

--- src/main/java/com/bbs/demo/HomeController.java
+++ src/main/java/com/bbs/demo/HomeController.java
@@ -2,16 +2,11 @@ package com.bbs.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-//@RestController
 
 @Controller
-public class HelloController {
-
+public class HomeController {
     @GetMapping("/")
     public String index() {
-        //return "Hello, 1조!";
-    	return "index"; //templates/index.html 랜더링
+        return "index";  // index.html 렌더링
     }
 }
