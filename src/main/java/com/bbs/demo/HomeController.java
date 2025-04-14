658a26608f016d8a605bc95561f37fc7285604b5
HaeungLee <smanew@hotmail.com>
2025-04-14 14:13:07 +0900

    팀html 1~6까지

--- src/main/java/com/bbs/demo/HomeController.java
+++ src/main/java/com/bbs/demo/HomeController.java
@@ -9,4 +9,33 @@ public class HomeController {
     public String index() {
         return "index";  // index.html 렌더링
     }
+    @GetMapping("/test01")
+    public String test01() {
+        return "TEST01";
+    }
+
+    @GetMapping("/test02")
+    public String test02() {
+        return "TEST02";
+    }
+
+    @GetMapping("/test03")
+    public String test03() {
+        return "TEST03";
+    }
+
+    @GetMapping("/test04")
+    public String test04() {
+        return "TEST04";
+    }
+
+    @GetMapping("/test05")
+    public String test05() {
+        return "TEST05";
+    }
+
+    @GetMapping("/test06")
+    public String test06() {
+        return "TEST06";
+    }
 }
