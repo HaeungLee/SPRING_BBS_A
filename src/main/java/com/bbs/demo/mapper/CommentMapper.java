4480bc3542ca1858312c652048456748797a852f
HaeungLee <smanew@hotmail.com>
2025-04-22 18:01:43 +0900

    comment 쪽 날림

--- src/main/java/com/bbs/demo/mapper/CommentMapper.java
+++ src/main/java/com/bbs/demo/mapper/CommentMapper.java
@@ -1,19 +1,6 @@
 package com.bbs.demo.mapper;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import com.bbs.demo.model.Comment;
-
-@Mapper
 public interface CommentMapper {
 	
-	List<Comment> getCommentsByPostId(@Param("postId") int postId);
-	int insertComment(Comment comment);
-    int updateComment(Comment comment);
-    int deleteComment(@Param("id") int id, @Param("userId") int userId);
-    Comment getCommentById(int id);
     
 }
--- src/main/java/com/bbs/demo/service/CommentService.java
+++ src/main/java/com/bbs/demo/service/CommentService.java
@@ -1,13 +1,5 @@
 package com.bbs.demo.service;
 
-import java.util.List;
-
-import com.bbs.demo.model.Comment;
-
 public interface CommentService {
     
-	List<Comment> getCommentsByPostId(int postId);
-	void createComment(Comment comment);
-	boolean updateComment(Comment comment, int userId);
-	boolean deleteComment(int id, int userId);
 }
