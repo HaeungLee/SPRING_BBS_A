97e97e2fdbd9fb014401b61686ed6fa98c99cfe3
HaeungLee <smanew@hotmail.com>
2025-04-22 16:07:07 +0900

    Member Mapper,model 추가

--- src/main/java/com/bbs/demo/mapper/MemberMapper.java
+++ src/main/java/com/bbs/demo/mapper/MemberMapper.java
@@ -1,5 +1,13 @@
 package com.bbs.demo.mapper;
 
-public class MemberMapper {
-    
+import com.bbs.demo.model.Member;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper {
+
+    Member findById(Long userId);
+    void insert(Member member);
+    void update(Member member);
+    void delete(Long userId);
 }
--- src/main/java/com/bbs/demo/model/Member.java
+++ src/main/java/com/bbs/demo/model/Member.java
@@ -1,5 +1,17 @@
 package com.bbs.demo.model;
 
-public class Member {
-    
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class Member {	
+	
+	private Long userId;
+    private String username;
+    private String password;
+    private String email;
+    private String nickname;
+    private String isManager;
+    private LocalDateTime createdAt;
 }
